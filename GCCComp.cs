7b575bfa1cf087696c17b558d4466c7ea84b09f6
Pyro569 <doggolover98@outlook.com>
2023-12-23 00:17:04 -0500

    .gitignore

--- GCCComp.cs
+++ GCCComp.cs
@@ -7,13 +7,17 @@ class GCCComp
         //check the operating system and adjust the commands ran based off that
         string OperatingSystem = System.Runtime.InteropServices.RuntimeInformation.OSDescription;
 
+        System.Threading.Thread.Sleep(500);
         Process.Start("gcc", "Main.cpp -w -lstdc++");
 
         try
         {
             //run the appropriate command based off the operating system that is being ran
             if (OperatingSystem.Contains("Linux"))
+            {
                 Process.Start("mv", "./a.out ./" + BinaryName);
+                //Process.Start("rm", "Main.cpp");
+            }
             else if (OperatingSystem.Contains("Windows"))
                 Process.Start("ren a.out " + BinaryName);
         }
--- obj/Release/net6.0/win-x64/Pine.AssemblyInfo.cs
+++ obj/Release/net6.0/win-x64/Pine.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb558bd337a58a112e5f4bf040613bbcb8bfbe58")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
